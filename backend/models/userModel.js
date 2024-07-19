f0902d9b2d51899cb960b28605fab317688f0b70
harshil1277 <kaneria.1@iitj.ac.in>
2024-07-19 23:46:03 +0530

    MondoDB Connected

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -7,7 +7,6 @@ const userSchema = new mongoose.Schema(
         password: { type: String, required: true },
         pic: {
             type: String,
-            required: true,
             default: 'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg'
         },
         isAdmin: { type: Boolean, required: true, default: false },
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,22 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const {chats} = require('./data/data');
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
 
+dotenv.config();
+connectDB();
 const app = express();
 
+app.use(express.json()); //to accept json data
 
 const PORT = process.env.PORT || 5000;
 
-
-
-app.get('/', (req, res) => {
-    res.send('API is running.... Properly');
+app.get("/", (req, res) => {
+  res.send("API is running.... Properly");
 });
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
-app.get('/api/chat/:id', (req, res) => {
-    // console.log(req.params.id);
-    const chat = chats.find((c) => c._id === req.params.id);
-    res.send(chat);
-});    
-
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold));
